19a3bb9f0a78d8532a61079d4d79f45957f2151b
Dongwook Kim <20143341@changwon.ac.kr>
2021-01-27 09:44:23 +0900

    Add files via upload

---
+++ 0x04/0x04_list.cpp
@@ -0,0 +1,24 @@
+﻿// 0x04_list.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main(void) {
+    list<int> L = { 1,2 }; // 1 2
+    list<int>::iterator t = L.begin(); // t는 1을 가리키는 중
+    L.push_front(10); // 10 1 2
+    cout << *t << '\n'; // t가 가리키는 값 = 1을 출력
+    L.push_back(5); // 10 1 2 5
+    L.insert(t, 6); // t가 가리키는 곳 앞에 6을 삽입, 10 6 1 2 5
+    t++; // t를 1칸 앞으로 전진, 현재 t가 가리키는 값은 2
+    t = L.erase(t); // t가 가리키는 값을 제거, 그 다음 원소인 5의 위치를 반환
+                    // 10 6 1 5, t가 가리키는 값은 5
+    cout << *t << '\n'; // 5
+    for (auto i : L) cout << i << ' ';
+    cout << '\n';
+    for (list<int>::iterator it = L.begin(); it != L.end(); it++)
+        cout << *it << ' ';
+}
